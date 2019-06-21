aaee4015ff85b9e1d3d944c6766a7ab8418e4343
Gowtham <gowtham.p@in.reportbee.com>
2019-06-21 08:19:57 +0530

    Row, Column, Navigate to another screen functionality

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,177 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Navigation Basics',
-    home: FirstRoute(),
+    home: MainRoute(),
   ));
 }
 
-class FirstRoute extends StatelessWidget {
+class MainRoute extends StatelessWidget {
   int count = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Route'),
+        title: Text('Learning App'),
       ),
       body: Center(
-        child: RaisedButton(
-            child: Text('Open route'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SecondRoute()),
-              );
-            }),
-      ),
-    );
+        child: Container(
+          child: Column(
+            children: [ 
+              Text(''),
+              RaisedButton(
+                // row wise routing
+                child: Text('Navigations'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => NavigationRoute()),
+                  );
+                }),
+              RaisedButton(
+                child: Text('Layouts'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LayoutRoute()),
+                  );
+                }),
+                RaisedButton(
+                child: Text('Data passing'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DataRoute()),
+                  );
+                })
+            ]
+          )
+        )
+        )
+      );
   }
 }
 
-class SecondRoute extends StatelessWidget {
+class LayoutRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Route"),
+        title: Text("Layout Tutorial"),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [ 
+              Text(''),
+              RaisedButton(
+                // row wise routing
+                child: Text('Row'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RowLayoutRoute()),
+                  );
+                }),
+              RaisedButton(
+                child: Text('Column'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ColumnLayoutRoute()),
+                  );
+                }),
+                RaisedButton(
+                child: Text('List View'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LayoutRoute()),
+                  );
+                }),
+                RaisedButton(
+                child: Text('Grid View'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LayoutRoute()),
+                  );
+                })
+            ]
+          )
+        )
+      )
+    );
+  }
+}
+
+class RowLayoutRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Row Layout "),
+      ),
+      body: Center(
+        child: Container(
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            children: [ 
+              RaisedButton(
+                child: Text('Row 1'),
+              ),
+              RaisedButton(
+                child: Text('Row 2'),
+              ),
+              RaisedButton(
+                child: Text('Row 3'),
+              ),
+            ]
+          )
+        )
+      )
+    );
+  }
+}
+
+class ColumnLayoutRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Column Layout "),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [ 
+              RaisedButton(
+                child: Text('Column 1'),
+              ),
+              RaisedButton(
+                child: Text('Column 2'),
+              ),
+              RaisedButton(
+                child: Text('Column 3'),
+              ),
+              RaisedButton(
+                child: Text('Column 4'),
+              ),
+            ]
+          )
+        )
+      )
+    );
+  }
+}
+
+class DataRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Layout Tutorial"),
       ),
       body: Center(
         child: RaisedButton(
@@ -48,3 +186,25 @@ class SecondRoute extends StatelessWidget {
     );
   }
 }
+
+class NavigationRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Navigation Tutorial"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+
+// for alignment refer
